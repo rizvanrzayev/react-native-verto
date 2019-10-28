fb47fe330bc2e7f52d3d6baf25d2e5d82075b9ac
Haqverdi Behbudov <haqverdib@gmail.com>
2019-10-28 10:46:42 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import VertoClient from './verto/VertoClient';
 import {generateGUID, ENUM} from './verto/utils';
 import VertoView from "./vertoView"
 
+// export all files
 export {VertoClient, generateGUID, ENUM, VertoView};
