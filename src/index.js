d271a92093d7318653cbb4e15579b19021b97b0c
Haqverdi Behbudov <haqverdib@gmail.com>
2019-10-28 10:42:43 +0400

    update index add console.log

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import VertoClient from './verto/VertoClient';
 import {generateGUID, ENUM} from './verto/utils';
 import VertoView from "./vertoView"
 
+console.log('test');
+
 export {VertoClient, generateGUID, ENUM, VertoView};
